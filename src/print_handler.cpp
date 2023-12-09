3f3feb079967d329de60ae57a0393ab0b8d2dbd0
bttlfwtr <JeffTan.b@gmail.com>
2023-12-09 15:21:50 -0800

    simplify menu commands

--- src/print_handler.cpp
+++ src/print_handler.cpp
@@ -2,14 +2,12 @@
 
 void PrintHandler::PrintMenu(std::ostream& outStringStream)
 {
-          outStringStream << "(A)dd\n"  // : add item to domain
-                    << "(S)earch\n"     // : search specific content
-                    << "(M)odify\n"     // : modify existing item information
-                    << "(D)elete\n"     // : delete item from domain
-                    << "(P)rint\n"      // : print all vault content
-                    << "(E)xit\n"       // : exit program
-                    << "(L)ogout\n"     // : exit and remove login from config
-                    << "(H)elp\n"       // : get choice instructions
+          outStringStream << "    (A)dd\n"  // : add item to domain
+                    << "    (S)earch\n"     // : search specific content, or print all content of vault
+                    << "    (M)odify\n"     // : modify existing item information
+                    << "    (D)elete\n"     // : delete item from domain
+                    << "    (E)xit\n"       // : exit program, E -l removes login cred from config
+                    << "    (H)elp\n"       // : get choice instructions
                     << "><>choice: ";
 
           return;
