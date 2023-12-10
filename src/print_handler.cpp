1eced3be9dc521725d7791737d85591672276da2
bttlfwtr <JeffTan.b@gmail.com>
2023-12-10 01:18:06 -0800

    change print format

--- src/print_handler.cpp
+++ src/print_handler.cpp
@@ -2,13 +2,13 @@
 
 void PrintHandler::PrintMenu(std::ostream& outStringStream)
 {
-          outStringStream << "    (A)dd\n"  // : add item to domain
-                    << "    (S)earch\n"     // : search specific content, or print all content of vault
-                    << "    (M)odify\n"     // : modify existing item information
-                    << "    (D)elete\n"     // : delete item from domain
-                    << "    (E)xit\n"       // : exit program, E -l removes login cred from config
-                    << "    (H)elp\n"       // : get choice instructions
-                    << "><>choice: ";
+          outStringStream << "(A)dd\n"  // : add item to domain
+                    << "(S)earch\n"     // : search specific content, or print all content of vault
+                    << "(M)odify\n"     // : modify existing item information
+                    << "(D)elete\n"     // : delete item from domain
+                    << "(E)xit\n"       // : exit program, E -l removes login cred from config
+                    << "(H)elp\n"       // : get choice instructions
+                    << "    ><>choice: ";
 
           return;
 }
