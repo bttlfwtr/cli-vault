2f0cf24552431bee094b92b7317947a3de4fe222
glssfwtr <jefftan.b@gmail.com>
2024-03-20 19:59:57 -0700

    comment alternative

--- src/Tag.cpp
+++ src/Tag.cpp
@@ -11,6 +11,7 @@ std::string TagFunctions::GetStringFromTag(const Tag& tag)
 }
 
 // iterate through every first to get its pair then check for its second's first
+// alternatively create a tag specifically for String -> Tag to use unordered_map find()
 Tag TagFunctions::GetTagFromString(const std::string& tagStr)
 {
       for (const auto& pair : biMapTagStringTable)
